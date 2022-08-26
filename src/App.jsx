5e83b6766b01314c5da53749b56a3112c509c208
Matias Aramayo <matiasaramayo@hotmail.com>
2022-08-26 14:16:45 -0300

    after class

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import ItemListContainer from './components/container/ItemListContainer/ItemListContainer'
+import Dolar from './components/single/Dolar/Dolar'
+import DolarCustomHook from './components/single/Dolar/DolarCustomHook'
+import ItemDetail from './components/single/ItemDetail/ItemDetail'
+
+function App() {
+
+  return (
+    <div className="App">
+        {/* <ItemListContainer/>
+        <ItemDetail/> */}
+        {/* <Dolar/> */}
+        {/* <DolarCustomHook/> */}
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/container/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+
+import ItemList from '../ItemList/ItemList'
+
+const ItemListContainer = () => {
+  
+
+  return (
+    <div>
+        <ItemList/>
+    </div>
+  )
+}
+
+export default ItemListContainer
